d2fda33384c11c5a55d605192606a4a34b7f6da8
Manfred Stienstra <manfred@fngtps.com>
2017-05-23 13:51:38 +0200

    Use assert_nil to test for nil.

--- test/unit/tedrahcu_test.rb
+++ test/unit/tedrahcu_test.rb
@@ -2,7 +2,7 @@ require_relative '../preamble'
 
 class TedrahcuTest < Minitest::Test
   def test_detects_charset
-    assert_equal nil, Tedrahcu.detect(nil)
+    assert_nil Tedrahcu.detect(nil)
     assert_equal '', Tedrahcu.detect('')
     assert_equal 'UTF-8', Tedrahcu.detect('é')
     assert_equal 'ASCII', Tedrahcu.detect('There are so many ASCII characters.')
