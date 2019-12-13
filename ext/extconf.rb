24df9d13673dfe755c8dc97f923f3030fc6e6ffc
Manfred Stienstra <manfred@fngtps.com>
2019-12-13 09:03:52 +0100

    Explain what to do when Uchardet can't be found.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,6 +1,22 @@
 require 'mkmf'
 
 dir_config('uchardet')
-$CFLAGS << ' ' + pkg_config('uchardet', 'cflags')
-$LDFLAGS << ' ' + pkg_config('uchardet', 'libs')
-create_makefile('tedrahcu', 'tedrahcu')
+if pkg_config('uchardet')
+  $CFLAGS << ' ' + pkg_config('uchardet', 'cflags')
+  $LDFLAGS << ' ' + pkg_config('uchardet', 'libs')
+  create_makefile('tedrahcu', 'tedrahcu')
+else
+  fail <<~MESSAGE
+    Could not find Uchardet build information using pkg-config.
+    Please make sure both pkg-config and Uchardet are installed. For example:
+    
+    ** macOS **
+    brew install pkg-config uchardet
+
+    ** CentOS **
+    yum install uchardet-devel
+
+    ** Debian **
+    apt install libuchardet-dev
+  MESSAGE
+end
