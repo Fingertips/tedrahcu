e456d0046df2d7788ece71febc4e9733c2f1bdc0
Manfred Stienstra <manfred@fngtps.com>
2023-12-11 10:34:22 +0100

    [Rubocop] Autocorrect memory.rb.

--- test/profile/memory.rb
+++ test/profile/memory.rb
@@ -1,3 +1,5 @@
-$:.unshift(File.expand_path("../../ext", __dir__))
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift(File.expand_path('../../ext', __dir__))
 require 'tedrahcu'
 Tedrahcu.detect(File.read('tedrahcu.gemspec'))
