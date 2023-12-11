eaa041e79d391761e7c1c90ebe5ba8da05eb558a
Manfred Stienstra <manfred@fngtps.com>
2023-12-11 10:33:54 +0100

    [Rubocop] Autocorrect extconf.rb.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,22 +1,22 @@
+# frozen_string_literal: true
+
 require 'mkmf'
 
 dir_config('uchardet')
-if pkg_config('uchardet')
-  $CFLAGS << ' ' + pkg_config('uchardet', 'cflags')
-  $LDFLAGS << ' ' + pkg_config('uchardet', 'libs')
-  create_makefile('tedrahcu', 'tedrahcu')
-else
-  fail <<~MESSAGE
-    Could not find Uchardet build information using pkg-config.
-    Please make sure both pkg-config and Uchardet are installed. For example:
-    
-    ** macOS **
-    brew install pkg-config uchardet
+raise <<~MESSAGE unless pkg_config('uchardet')
+  Could not find Uchardet build information using pkg-config.
+  Please make sure both pkg-config and Uchardet are installed. For example:
 
-    ** CentOS **
-    yum install uchardet-devel
+  ** macOS **
+  brew install pkg-config uchardet
 
-    ** Debian **
-    apt install libuchardet-dev
-  MESSAGE
-end
+  ** CentOS **
+  yum install uchardet-devel
+
+  ** Debian **
+  apt install libuchardet-dev
+MESSAGE
+
+$CFLAGS << " #{pkg_config('uchardet', 'cflags')}"
+$LDFLAGS << " #{pkg_config('uchardet', 'libs')}"
+create_makefile('tedrahcu', 'tedrahcu')
